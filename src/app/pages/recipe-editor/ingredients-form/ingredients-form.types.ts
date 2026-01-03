35ac5fbd9ec5050545d5f167dea019a613ae8b77
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-01-03 08:06:23 -0600

    Defines new type for plain ingredient data

--- src/app/pages/recipe-editor/ingredients-form/ingredients-form.types.ts
+++ src/app/pages/recipe-editor/ingredients-form/ingredients-form.types.ts
@@ -9,3 +9,11 @@ export interface IngredientFormGroup {
 
 // Each row is a FormGroup of above controls
 export type IngredientRow = FormGroup<IngredientFormGroup>;
+
+// Plain object value of an ingredient row
+export interface IngredientValue {
+  name: string;
+  quantity: string;
+  unit: string;
+  isEditing: boolean;
+}
