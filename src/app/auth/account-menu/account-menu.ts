d630a72b4963a34da2fabb5d6c9e605919ff41ff
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-01-23 18:02:42 -0600

    Generate separate component for user menu

---
+++ src/app/auth/account-menu/account-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-menu',
+  imports: [],
+  templateUrl: './account-menu.html',
+  styleUrl: './account-menu.css',
+})
+export class AccountMenu {
+
+}
