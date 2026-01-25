7b34043231255830aecddea0e940610bc87ec987
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-01-24 18:54:58 -0600

    cleanup

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { AuthService } from '../../services/auth.service';
 import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, MatIconModule, MatMenu, MatMenuTrigger],
+  imports: [
+    CommonModule, 
+    MatIconModule, 
+    RouterModule,
+    MatMenu, 
+    MatMenuTrigger
+  ],
   standalone: true,
   templateUrl: './login.html',
   styleUrl: './login.css'
