368dccd1740c618b094f14bebb6352b0376aede3
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-02-04 06:11:50 -0600

    Assign authService observables to local vars

--- src/app/auth/account-menu/account-menu.ts
+++ src/app/auth/account-menu/account-menu.ts
@@ -5,6 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 import { AuthService } from '../../services/auth.service';
 import { User } from 'firebase/auth';
+import { UserProfile } from '../../models/user-profile.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-account-menu',
@@ -21,16 +23,17 @@ import { User } from 'firebase/auth';
 export class AccountMenu implements OnInit {
   @Output() requestClose = new EventEmitter<void>();
 
-  user: User | null = null;
+  // Creates references to observables set in AuthService
+  user$: Observable<User | null>;
+  userProfile$: Observable<UserProfile | null>;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    this.user$ = this.authService.authState$;
+    this.userProfile$ = this.authService.userProfile$;
+  }
 
   // Called after input properties are set, but before DOM is ready
-  ngOnInit() {
-    this.authService.authState$.subscribe(user => {
-      this.user = user;
-    });
-  }
+  ngOnInit() {}
 
   triggerCloseMenu() {
     try {
@@ -43,7 +46,6 @@ export class AccountMenu implements OnInit {
   async handleSignOut() {
     try {
       await this.authService.signOut();
-      this.user = null;
       console.log("Signed out");
     } catch (err) {
       console.log("Sign-out error:", err);
