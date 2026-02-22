546d5206de522108dc7eee9d839698083a3f2305
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-02-22 15:42:51 -0600

    Create TagsForm component

---
+++ src/app/pages/recipe-editor/tags-form/tags-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tags-form',
+  imports: [],
+  templateUrl: './tags-form.html',
+  styleUrl: './tags-form.css',
+})
+export class TagsForm {
+
+}
