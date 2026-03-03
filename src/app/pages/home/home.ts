e2a65caedfe8687157864ae3f6aa79dbdf8f5a2e
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-03-03 09:32:01 -0600

    Customize homepage for guest/auth

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from 'firebase/auth';
+import { Observable } from 'rxjs';
+import { UserProfile } from '../../models/user-profile.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
-  imports: [RouterModule]
+  imports: [CommonModule, RouterModule]
 })
 export class Home {
+  user: User | null = null;
+  userProfile$!: Observable<UserProfile | null>;
 
+  constructor(private authService: AuthService) {
+    this.userProfile$ = this.authService.userProfile$;
+
+    this.authService.authState$.subscribe(user => {
+      this.user = user;
+    });
+  }
 }
