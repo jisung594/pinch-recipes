82ee0e38cda1b2b98f19b6569b439369cd28026b
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-03-21 14:08:37 -0500

    Add break

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -40,7 +40,7 @@ export class Home implements OnInit {
       if (profile) {
         fullText = 'Hi,' + '\n' + (profile?.displayName || profile?.firstName || 'maker') + '.';
       } else {
-        fullText = 'Recipes, by you.';
+        fullText = 'Recipes,' + '\n' + 'by you.';
       }
       this.typeText(fullText);
     });
