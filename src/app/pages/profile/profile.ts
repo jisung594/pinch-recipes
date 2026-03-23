cf3c46a5b76cd190d5b70390a0a8107bba2c95f1
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-03-23 12:54:20 -0500

    refactor: remove recipe functionality from profile

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,15 +1,13 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { Component, OnInit, OnDestroy } from '@angular/core';
 import { User } from 'firebase/auth';
-import { Observable, Subscription, of, switchMap } from 'rxjs';
-import { Recipe } from '../../models/recipe.model';
-import { AuthService } from '../../services/auth.service';
-import { RecipeFirestoreService } from '../../services/recipe-firestore.service';
+import { Observable, Subscription } from 'rxjs';
 import { ToastService } from '../../services/toast.service';
 import { ProfileForm } from '../profile/profile-form/profile-form';
 import { UserProfile } from '../../models/user-profile.model';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -19,43 +17,21 @@ import { UserProfile } from '../../models/user-profile.model';
   styleUrl: './profile.css',
 })
 export class Profile implements OnInit, OnDestroy {
-  // $ (syntax for observable)
-  // ! (not initialized in constructor, but
-  //   promises to assign value before accessed)
   isEditingProfile = false;
   editedProfileData?: UserProfile;
-  private currentUser?: User | null;
-  recipes$!: Observable<Recipe[]>;
   userProfile$!: Observable<UserProfile | null>;
-  mainRecipes: Recipe[] = [];
-  archivedRecipes: Recipe[] = [];
+  private currentUser?: User | null;
   private subscriptions = new Subscription();
 
   constructor(
     private authService: AuthService,
-    private firestoreService: RecipeFirestoreService,
     private toastService: ToastService,
   ) {}
 
   async ngOnInit() {
-    const sub = this.authService.authState$
-      .pipe(
-        switchMap((user) => {
-          this.currentUser = user;
-
-          if (!user) {
-            return of([]);
-          }
-
-          return this.firestoreService.getUserRecipes(user.uid);
-        }),
-      )
-      .subscribe((recipes) => {
-        this.mainRecipes = recipes.filter((recipe) => recipe.archived === false);
-        this.archivedRecipes = recipes.filter(
-          (recipe) => recipe.archived === true || recipe.archived === undefined,
-        );
-      });
+    const sub = this.authService.authState$.subscribe((user) => {
+      this.currentUser = user;
+    });
 
     this.subscriptions.add(sub);
   }
@@ -81,7 +57,6 @@ export class Profile implements OnInit, OnDestroy {
         this.isEditingProfile = false;
       }
 
-      // Toast notification upon update
       this.toastService.notify('Profile updated successfully.');
     } catch (err) {
       console.error('Error updating profile:', err);
